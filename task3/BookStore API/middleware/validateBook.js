b9d55d1216eed2185f54a496d67e07e1ced46983
Abdissa Habtamu <abdissahabtamu27@gmail.com>
2025-12-23 23:01:50 +0300

    task done

---
+++ task3/BookStore API/middleware/validateBook.js
@@ -0,0 +1,16 @@
+import Joi from "../controllers/node_modules/joi/lib";
+
+const bookSchema = Joi.object({
+  title: Joi.string().required(),
+  price: Joi.number().min(0).required(),
+});
+
+export const validateBook = (req, res, next) => {
+  const { error } = bookSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  next();
+};
